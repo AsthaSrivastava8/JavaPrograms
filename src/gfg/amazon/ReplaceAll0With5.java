a123889f774fb9042af48f6242fc83f8ab1fd69f
Astha <srivastavastha811@gmail.com>
2021-09-09 22:52:51 +0530

    numbers

--- src/gfg/amazon/ReplaceAll0With5.java
+++ src/gfg/amazon/ReplaceAll0With5.java
@@ -1,5 +1,30 @@
 package gfg.amazon;
 
+import java.util.Scanner;
+
 public class ReplaceAll0With5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int newNumber = 0;
+        int temp = number;
+        while (temp > 0) {
+            int rem = temp % 10;
+            if (rem == 0)
+                newNumber = newNumber * 10 + 5;
+            else
+                newNumber = newNumber * 10 + rem;
+            temp = temp / 10;
+        }
+        number = newNumber;
+        temp = number;
+        newNumber = 0;
+        while (temp > 0) {
+            newNumber = newNumber *10 + (temp%10);
+            temp /= 10;
+        }
+
+        System.out.println(newNumber);
+    }
 
 }
